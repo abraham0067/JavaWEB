948a4cfe4b223c3864bb9fadd617c6307f9fafe8
Abraham <abraham0067@gmail.com>
2018-02-23 11:14:50 -0600

    Cambio en la prueba4

--- src/prueba1/testtest.java
+++ src/prueba1/testtest.java
@@ -4,6 +4,9 @@ public class testtest {
     public static void main(String[] args) {
         try {
             System.out.println("Hellow world");
-        }catch (Exception ex){}
+        }catch (Exception ex){
+            ex.printStackTrace(System.out);
+            System.out.println("Prueba exitosa");
+        }
     }
 }
