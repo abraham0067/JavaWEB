6e440aa87d8fdd7d948439dd649cf305827f84ad
Abraham <abraham0067@gmail.com>
2018-02-23 11:10:28 -0600

    Cambio en la prueba3

--- src/prueba1/testtest.java
+++ src/prueba1/testtest.java
@@ -3,7 +3,7 @@ package prueba1;
 public class testtest {
     public static void main(String[] args) {
         try {
-            System.out.println("args = " + args);
+            System.out.println("Hellow world");
         }catch (Exception ex){}
     }
 }
