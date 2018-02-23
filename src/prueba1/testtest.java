0f8fa21a75ad8df1128155ee1ff3bfb81f8714e3
Abraham <abraham0067@gmail.com>
2018-02-23 10:48:18 -0600

    Initial commit

---
+++ src/prueba1/testtest.java
@@ -0,0 +1,9 @@
+package prueba1;
+
+public class testtest {
+    public static void main(String[] args) {
+        try {
+            System.out.println("args = " + args);
+        }catch (Exception ex){}
+    }
+}
