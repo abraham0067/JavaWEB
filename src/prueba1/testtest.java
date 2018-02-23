287ebd41181250a62b6260c86e1bbb809f6c70b0
Abraham <abraham0067@gmail.com>
2018-02-23 10:58:04 -0600

    Cambio en la prueba2

--- src/prueba1/testtest.java
+++ src/prueba1/testtest.java
@@ -3,6 +3,7 @@ package prueba1;
 public class testtest {
     public static void main(String[] args) {
         try {
+            //NO ESTA JALANDO
             System.out.println("args = " + args);
         }catch (Exception ex){ex.printStackTrace(System.out);}
     }
